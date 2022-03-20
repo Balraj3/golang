231eec52c7438b54418f70285bcd6b9c3958d27d
Balraj A <71279902+Balraj3@users.noreply.github.com>
2022-03-20 21:37:49 +0530

    Update copy_without_routine.go

--- copy_without_routine.go
+++ copy_without_routine.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
 	"net/url"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,8 +11,12 @@ import (
 )
 
 func main() {
+	profile := flag.String("p", "defualt", "Mentoin profile")
+	source_buc := flag.String("s", "", "Enter source bucket name")
+	dest_buc := flag.String("d", "", "Enter destination bucket name")
+	flag.Parse()
 	sess, err := session.NewSessionWithOptions(session.Options{
-		Profile:           "dev",
+		Profile:           *profile,
 		SharedConfigState: session.SharedConfigEnable,
 	})
 	if err != nil {
@@ -21,13 +25,13 @@ func main() {
 	}
 	svc := s3.New(sess)
 	//get s3 items to resp
-	source := "taskawsbucket-hosting"
-	other := "task-dest"
-	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(source)})
+	// source := "taskawsbucket-hosting"
+	// other := "task-dest"
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(*source_buc)})
 	for _, item := range resp.Contents {
 		//fmt.Println("Name:         ", *item.Key)
-		ob1 := source + "/" + (*item.Key)
-		_, err = svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(other),
+		ob1 := *source_buc + "/" + (*item.Key)
+		_, err = svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(*dest_buc),
 			CopySource: aws.String(url.PathEscape(ob1)), Key: aws.String(*item.Key)})
 		//fmt.Println("Copying")
 	}
