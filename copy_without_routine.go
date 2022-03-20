4fac0cea39744e73127e5f3f223232bb940fc239
Balraj A <71279902+Balraj3@users.noreply.github.com>
2022-03-20 21:38:34 +0530

    Update copy_without_routine.go

--- copy_without_routine.go
+++ copy_without_routine.go
@@ -24,16 +24,11 @@ func main() {
 
 	}
 	svc := s3.New(sess)
-	//get s3 items to resp
-	// source := "taskawsbucket-hosting"
-	// other := "task-dest"
 	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(*source_buc)})
 	for _, item := range resp.Contents {
-		//fmt.Println("Name:         ", *item.Key)
 		ob1 := *source_buc + "/" + (*item.Key)
 		_, err = svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(*dest_buc),
 			CopySource: aws.String(url.PathEscape(ob1)), Key: aws.String(*item.Key)})
-		//fmt.Println("Copying")
 	}
 
 }
